f0840b378ce0316295d38e67cdcde8ce80533b3e
OleksiiVirsta <virstaaleksej@gmail.com>
2025-03-21 11:48:42 +0200

    (fix) Homework 3.1

---
+++ L3/L3.1.py
@@ -0,0 +1,17 @@
+#калькулятор
+a = int(input('First number: '))
+b = input('Operations: ')
+c = int(input('Second number: '))
+# if b == '/' and c == 0:
+#     print('Error zerro')
+
+if b == '+':
+    print(a, b, c, '=', (a+c))
+elif b == '-':
+    print(a, b, c, '=', (a-c))
+elif b == '*':
+    print(a, b, c, '=', (a*c))
+elif b == '/':
+    print(a, b, c, '=', (a / c if c != 0 else 'На нуль ділити не можна'))
+else:
+    print ("Ви ввели щось не те!")
