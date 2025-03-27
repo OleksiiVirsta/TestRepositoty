f7a6eaa8202d12e7ed569a24d706b76fa7f094a1
OleksiiVirsta <virstaaleksej@gmail.com>
2025-03-27 11:19:58 +0200

    fix Homework 4.2

--- L4/L4.1.py
+++ L4/L4.1.py
@@ -1,21 +1,9 @@
 # З циклом for
-# lst = [0, 5, 8, 15, 45, 145, 0, 78, 0, 122]
-# print(lst)
-#
-# for i in lst:
-#     if i == 0:
-#         a = lst.remove(i)
-#         lst.append(0)
-# print(lst)
-
-# З циклом while
-lst = [0, 5, 8, 15, 45, 145, 0, 78, 0, 122]
+lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 0, 0, 96, 0]
 print(lst)
-i = 0
-while i < len(lst):
-    l = lst[i]
-    if l == 0:
-        a = lst.pop(i)
-        lst.append(a)
-    i += 1
+
+for i in lst:
+    if i == 0:
+        a = lst.remove(i)
+        lst.append(0)
 print(lst)
