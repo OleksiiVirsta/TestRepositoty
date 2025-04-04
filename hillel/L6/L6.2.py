c58d36d33e72bdc25b55578f07031d960fa7c469
OleksiiVirsta <virstaaleksej@gmail.com>
2025-04-04 20:49:16 +0300

    fix Homework 6.2

--- hillel/L6/L6.2.py
+++ hillel/L6/L6.2.py
@@ -3,12 +3,13 @@ d_t = int(input('Enter seconds: '))
 days, d = divmod(d_t, 86400)
 hour, h = divmod(d, 3600)
 min, m = divmod(h, 60)
-sec = d_t - ((days * 86400) + (hour * 3600) + (min * 60))
 
 text = ' днів, '
 if str(days)[-1] == '1' and days != 11:
     text = ' день, '
-if str(days)[-1] == '2':
+elif 5<= days <= 20:
+    text = ' днів, '
+elif 2<= days%10 <=4:
     text = ' дні, '
 
-print(days, text, str(hour).zfill(2), ':', str(min).zfill(2), ':', str(sec).zfill(2), sep='')
+print(days, text, str(hour).zfill(2), ':', str(min).zfill(2), ':', str(m).zfill(2), sep='')
