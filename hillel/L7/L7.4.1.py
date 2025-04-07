785ce8437a952783109301301df8eada106e9ba1
OleksiiVirsta <virstaaleksej@gmail.com>
2025-04-07 12:15:32 +0300

    Homework 7.4.1

---
+++ hillel/L7/L7.4.1.py
@@ -0,0 +1,12 @@
+import random
+
+def common_elements():
+    r = [random.randrange(0, 100, 3) for i in range(random.randint(0, 100))]
+    d = [random.randrange(0, 100, 5) for i in range(random.randint(0, 100))]
+    set1 = set(r)
+    set2 = set(d)
+    inter = set1.intersection(set2)
+
+    return (inter)
+
+print(common_elements())
